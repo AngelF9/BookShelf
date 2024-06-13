9925333541a6a7e0c7cdd921f2420a3e7fe2dff2
Angel Fuentes <angelfc@csu.fullerton.edu>
2024-06-13 14:29:47 -0700

    first commit

---
+++ server/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  "mongodb+srv://angelf0515:sVXexNI6BaRIhcgj@books-store-mern.9hymdzx.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-Store-Mern";
